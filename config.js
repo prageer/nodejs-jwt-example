69b1dd827a40b993ded57b3a1608b7b14ff50317
prageer <prageer@cheerful.com>
2018-01-01 15:21:22 +0600

    app.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	'secret': 'SeCrEtKeYfOrHaShInG',
-	'mongodbUrl': 'mongodb://ghost_plan:ghost_plan@ds237947.mlab.com:37947/jwt-tutorial-prageer'
+	'mongodbUri': 'mongodb://ghost_plan:ghost_plan@ds237947.mlab.com:37947/jwt-tutorial-prageer'
 }
